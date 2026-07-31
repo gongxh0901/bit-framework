597298631b09e5e7fb601019f27069c4bd7f7359
gongxh <gongxinhai@kunpo.cc>
2026-07-31 17:22:17 +0800

    feature: bit-condition 导出 ConditionBase

--- bit-condition/src/index.ts
+++ bit-condition/src/index.ts
@@ -2,3 +2,5 @@ export * from './ConditionDecorator';
 export * from './ConditionManager';
 export * from './ConditionMode';
 export * from './ConditionModule';
+export * from './node/ConditionBase';
+
